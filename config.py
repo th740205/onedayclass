4ec3e0587f7ed37ece93f6ec7825acdaac112ee2
2dam2 <dami0674@gmail.com>
2025-10-13 12:00:57 +0900

    ldm1013

--- config.py
+++ config.py
@@ -8,4 +8,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
 WTF_CSRF_SECRET_KEY = os.environ.get("WTF_CSRF_SECRET_KEY", SECRET_KEY)  # (Flask-WTF 쓸 때)
 
-SECRET_KEY = "dev"
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
+MAX_CONTENT_LENGTH = 10 * 1024 * 1024
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# SECRET_KEY = "dev"
